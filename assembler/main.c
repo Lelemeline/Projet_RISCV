d5ad658067eeaaac252888be0195639e1a656d7b
Lelemeline <leameline.tavier@gmail.com>
2023-11-20 10:06:30 +0100

    first attempt of main

--- assembler/main.c
+++ assembler/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
@@ -13,11 +14,19 @@ int main(int argc, char **argv)
 
     /* ouvre le fichier d’entrée en mode lecture et le fichier de
     sortie en mode écriture, puis les ferme à la fin du un programme. */
+
     FILE *fe ; // fe = fichier d'entrée
     FILE *fs ; // fs = fichier de sortie
     fe = fopen(asm_input_file,"r");
     fs = fopen(hex_output_file,"w");
+
     /*programme*/
+    char *buffer = NULL;
+    size_t buf_size = 20;
+    buffer = malloc(buf_size*sizeof(char));
+    getline(&buffer,&buf_size,fe);
+    printf("%s\n",buffer);
+
     fclose(fe);
     fclose(fs);
     return 0;
