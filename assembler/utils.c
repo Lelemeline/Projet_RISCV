96f4504c81f474cddce21e773051aa5084e82fdf
volkroz <150960527+volkroz@users.noreply.github.com>
2023-11-27 11:18:58 +0100

    Update utils.c -ajout de trtmt_rgstr

--- assembler/utils.c
+++ assembler/utils.c
@@ -77,3 +77,15 @@ char *identification(char *line){
     free(cons);
     return line ;
 }
+void trtmt_rgstr(char *registre){ // dans cette fonction on cherche à prendre en charges les 2 types de notation des registres pour par la suite du programme n'utilisé que les allias
+     static char variables[32][4] = {"zero","ra","sp","gp","tp","t0","t1","t2","s0","s1","a0","a1","a2","a3","a4","a5","a6","a7","s2","s3","s4","s5","s6","s7","s8","s9","s10","s11","t3","t4","t5","t6"};
+     if (registre[0]=='x'){
+          if (strlen(registre)==3){
+               increment= 10*registre[1]+registre[2];
+          }
+          else {
+               increment=registre[1];
+          }
+          registre = variables[increment];
+     }
+}
