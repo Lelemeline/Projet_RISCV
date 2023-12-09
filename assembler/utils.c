3a5c87a75822dbf788ee13298d9abac67be1eced
Lelemeline <leameline.tavier@gmail.com>
2023-12-09 17:24:57 +0100

    ajout de commentaires

--- assembler/utils.c
+++ assembler/utils.c
@@ -1,5 +1,6 @@
 #include "utils.h"
 
+// déclaration des variables globales
 char instruction[13][5] = {"add","sub","addi","ld","sd","beq","bne","blt","bge","jal"} ;
 char registre[32][5] = {"zero","ra","sp","gp","tp","t0","t1","t2","s0","s1","a0","a1","a2","a3","a4","a5","a6","a7","s2","s3","s4","s5","s6","s7","s8","s9","s10","s11","t3","t4","t5","t6"} ;
 char type_instr[5] = {'R','I','S','B','J'};
--- assembler/utils.h
+++ assembler/utils.h
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include <ctype.h>
 
+// structure d'instruction de base
 typedef struct{
     int format ;
     uint32_t opcode  ;
@@ -14,12 +15,12 @@ typedef struct{
 } Instruction ;
 
 /* déclaration de fonctions */
-char *normalisation(char *line);// permet d'enlever les virgules
-void rcp_instr(char *line, char *cons); // récupération de l'instruction
-uint32_t identification(char *line); // identification de l'instruction
-void recup_arg(int a, char *line,char tab[2][10]);// récupère trois arguments
+char *normalisation(char *line);
+void rcp_instr(char *line, char *cons);
+void recup_arg(int a, char *line,char tab[2][10]);
 void concatener(char *line, const char *st1, const char *st2);
 int find_index_char(char *tab, char instr, int index);
 int find_index_string(char tab[32][5], char *instr) ;
 int normalisation_rgstr(char *tab);
 uint32_t assemble(Instruction a);
+uint32_t identification(char *line);
