82cbcc7924b02a9e835fc33922043d328f4ec60a
volkroz <150960527+volkroz@users.noreply.github.com>
2023-12-15 21:17:43 +0100

    Update j_offset-simple.s

--- tests/j_offset-simple.s
+++ tests/j_offset-simple.s
@@ -1,19 +1,20 @@
 li x1,1
 li x2,2
-j 2046
+j 12
 #commentaire
 #commentaire
 add x3,x1,  x2
     add x4,zero,x1
 li x5,5
-jal x0, -6
 li x6,6
 li x7,7
 
-j -16
+j -80000
+j 80000
 
 #EXPECTED
 #x1:1
 #x2:2
+#x5:5
 #x6:6
 #x7:7
