e00d49af50202d645e11c784c52d15ad40fbe16d
Lelemeline <leameline.tavier@gmail.com>
2023-12-15 10:32:29 +0100

    béton contre espace

--- tests/beq,bge,bne,blt.s
+++ tests/beq,bge,bne,blt.s
@@ -15,15 +15,15 @@ addi x7, x0, 3  # sautée si blt est vrai
 bge x3, x4, 0   # Sautera à la prochaine instruction si x3 est supérieur ou égal à x4
 addi x8, x0, 4  # sautée si bge est vrai
 
-# test pour si les conditions pour les instructions ne sont pas vérifiés 
-beq x1, x2, 0   
-addi x9, x0, 1  
-bne x1, x3, 0   
-addi x10, x0, 2  
-blt x2, x1, 0   
-addi x11, x0, 3  
-bge x4, x3, 0   
-addi x12, x0, 4  
+# test pour si les conditions pour les instructions ne sont pas vérifiés
+beq x1, x2, 0
+addi x9, x0, 1
+bne x1, x3, 0
+addi x10, x0, 2
+blt x2, x1, 0
+addi x11, x0, 3
+bge x4, x3, 0
+addi x12, x0, 4
 
 
 # EXPECTED
