08b38df107ccb9b238357d87c02f6464fa50599d
volkroz <150960527+volkroz@users.noreply.github.com>
2024-01-07 22:43:49 +0100

    Update beq,bge,bne,blt.s -correction

--- tests/beq,bge,bne,blt.s
+++ tests/beq,bge,bne,blt.s
@@ -1,44 +1,45 @@
 
 # Initialisation des valeurs dans les registres
-li  x1, 10
-li  x2, 20
+li  x16, 10
+li  x28, 20
 li  x3, 10
 li  x4, 5
 
 
 # Comparaison pour tester les instructions de branchement
-beq x1, x3, 8   # Sautera une instruction si x1 est égal à x3
+beq x16, x3, 8   # Sautera une instruction si x16 est égal à x3
 addi x5, x0, 1  # instruction sautée si l'instruction si dessus est vrai
-bne x2, x4, 8   # Sautera une instruction si x2 n'est pas égal à x4
+bne x28, x4, 8   # Sautera une instruction si x28 n'est pas égal à x4
 addi x6, x0, 2  # sautée si bne est vrai
-blt x1, x2, 8   # Sautera une instruction si x1 est inférieur à x2
+blt x16, x28, 8   # Sautera une instruction si x16 est inférieur à x28
 addi x7, x0, 3  # sautée si blt est vrai
 bge x3, x4, 8  # Sautera une instruction si x3 est supérieur ou égal à x4
 addi x8, x0, 4  # sautée si bge est vrai
 
 # test pour si les conditions pour les instructions ne sont pas vérifiés
 # debeugage qui ne peut être fait qu'en ayant les valeurs des registres
-beq x1, x2, 4
+beq x16, x28, 4
 addi x9, x0, 1
-bne x1, x3, 4
+bne x16, x3, 4
 addi x10, x0, 2
-blt x2, x1, 4
+blt x28, x16, 4
 addi x11, x0, 3
 bge x4, x3, 4
 addi x12, x0, 4
 
-beq x1,x13,-4096
-bne x2,x4,-4096
-blt x1,x2,-4096
+beq x16,x13,-4096
+bne x28,x4,-4096
+blt x16,x28,-4096
 bge x3,x4,-4096
-beq x1,x13,4094
-bne x2,x4,4094
-blt x1,x2,4094
+beq x16,x13,4094
+bne x28,x4,4094
+blt x16,x28,4094
 bge x3,x4,4094
 
 # EXPECTED
-# x1 :10
-# x2 :20
+#sp: 16384
+# x16 :10
+# x28 :20
 # x3 :10
 # x4 :5
 # x9 :1
