e9c1abd0d042c9f7d5113e8fe3ff6e8c33468a48
volkroz <150960527+volkroz@users.noreply.github.com>
2024-01-08 11:06:23 +0100

    Update jal-simple.s -corr

--- tests/jal-simple.s
+++ tests/jal-simple.s
@@ -5,13 +5,10 @@
     addi x11, x0, 20  
     li x16,1
     li x28,2
-
-    jal x6,-12000 #pour tester des grosses valeurs
-    jal x6,12000
-
+    
 # EXPECTED 
 #x2: 16384
-#x6: 12
+#x6: 4
 #x11:20
 #x16:1
 #x28:2
